cd09c2fb27ee331af5de68c8b66cdad5c91d986c
Guhan <guhan.shyam.iyer05@gmail.com>
2025-06-23 16:08:00 -0400

    refresh

--- src/weight_memory.v
+++ src/weight_memory.v
@@ -3,30 +3,37 @@
 module weight_memory(
     input wire clk,
     input wire rst,
-    input wire [15:0] addr,
-    output wire [7:0] w1,
-    output wire [7:0] w2,
-    output wire [7:0] w3,
-    output wire [7:0] w4
+    input wire mem_ctrl_en,
+    input wire [1:0] addr,
+    input reg [7:0] rpi_weights,
+    output reg [7:0] weight_1,
+    output reg [7:0] weight_2,
+    output reg [7:0] weight_3,
+    output reg [7:0] weight_4
 );
 
-reg [7:0] mem [7:0];
+reg [7:0] intermediate_memory [3:0];
 integer i;
 
 always @(posedge clk) begin
     if (rst) begin
         for (i = 0; i < 8; ++i) begin
-            mem[i] <= 8'b0;
+            intermediate_memory[i] <= 0;
         end
-        w1 <= 8'b0;
-        w2 <= 8'b0;
-        w3 <= 8'b0;
-        w4 <= 8'b0;
+        weight_1 <= 8'b0;
+        weight_2 <= 8'b0;
+        weight_3 <= 8'b0;
+        weight_4 <= 8'b0;
     end
-    w1 <= mem[addr];
-    w2 <= mem[addr+1];
-    w3 <= mem[addr+2];
-    w4 <= mem[addr+3];
+
+    if (mem_ctrl_en) begin
+        intermediate_memory[addr] <= rpi_weights;
+    end
+
+    weight_1 <= intermediate_memory[addr];
+    weight_2 <= intermediate_memory[addr + 1];
+    weight_3 <= intermediate_memory[addr + 2];
+    weight_4 <= intermediate_memory[addr + 3];
 end
 
 endmodule
