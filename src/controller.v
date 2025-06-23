17ef57a941e8466f90ba5b390f788d6204d75c64
William Zhang <wzhang20@yahoo.com>
2025-06-22 20:19:05 -0400

    more fixes

--- src/controller.v
+++ src/controller.v
@@ -1,16 +1,13 @@
 module controller (
     input  wire        clk,
+    input  wire        rst,
     input  wire        load_en,
     input  wire        load_sel_ab,
     input  wire [1:0]  load_index,
     input  wire [7:0]  in_data,
 
-    output logic [7:0] a_matrix [0:3],
-    output logic [7:0] b_matrix [0:3],
-
     input  wire        output_en,
     input  wire [1:0]  output_sel,
-    input  wire [7:0]  c_matrix [0:3],
     output wire [7:0]  out_data,
 
     output wire        done
@@ -18,11 +15,19 @@ module controller (
 
     logic start;
     logic started;
-    logic [3:0] a_loaded, b_loaded;
+
+    logic [7:0] c_matrix [0:3],
+
+    logic [7:0] a_matrix [0:3]; // the inputs themselves
+    logic [7:0] b_matrix [0:3];
+
+    integer i;
+
+    logic [3:0] a_loaded, b_loaded; // confirming loads of matrices for safe multiplication
 
     mmu array_inst (
         .clk(clk),
-        .rst(start),
+        .rst(~start),
         .A(a_matrix),
         .B(b_matrix),
         .C(c_matrix),
@@ -30,7 +35,17 @@ module controller (
     );
 
     always_ff @ (posedge clk) begin
-        if (load_en) begin
+        if (rst) begin
+            for (i = 0; i < 4; i++) begin
+                a_matrix[i] <= 8'b0;
+                b_matrix[i] <= 8'b0;
+                c_matrix[i] <= 8'b0;
+            end
+            a_loaded <= 4'b0;
+            b_loaded <= 4'b0;
+            start <= 0;
+            started <= 0;
+        end else if (load_en) begin
             if(!load_sel_ab) begin
                 a_matrix[load_index] <= in_data;
                 a_loaded[load_index] <= 1;
